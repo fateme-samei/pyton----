c3d060f3699f96ae7e83bd315cbe6195f437da87
fatemeh-samei <fati09156090@gmail.com>
2022-02-22 10:05:48 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+# print("hello world!")
+# from tkinter import Y
+
+
+# x =3
+# y=4
+# if x>y 
+#     print("yes")
+# else :
+#     print("no")    
+# print("سنتو وارد کن")
+# x = input()
+# print = (x)
+# x=9
+# if (x%2==0):
+#     print("even")
+# else :
+#     print("old")
+# my_list= [3,5,8]
+# for x in my_list :
+#     print(max(my_list))
+# print("num:")
+# x =input()
+# y =input()
+# z=input()
+# print(max(x,y,z))
+# my = [int(input()),int(input()),int(input())]
+# for x in my :
+#     print(max(my))
+# x = ()
+# print(type(x))
+# print(89//9)
+# w,z=3,7
+# print(w)
+# mynum=100
+# mynum=mynum-4 
+# print(mynum)
+# x="er"
+# y="gha"
+# z=int(input())
+# print("...............")
+# #role
+# print("masahat motavazi azla chegadr ast?")
+# x=float(input("height:")) * float(input("ryle:"))
+# x=round(x,2)
+# print(f"area : {x}cm")
+# x=1
+# print("yes")if x==1 else print("no")
+# my_list=[input(),input(),input(),input(),input()]
+# my_list.sort()
+# print(my_list)
+# x=1
+# y=2
+# print(f"ok: {y==x} ")
+# age=56
+# if (0<age<2 and age<=8):
+#     print("2dolar")
+# elif(age>=50) :
+#     print("5dolar")  
+# elif(not (0<age<2 and age<=8) or (age>=50) )  :
+#     print("10dolar")   
+# x=range(1,9,)
+# print(list(x))
+# pasword=input("pasword:")
+# while pasword != "1234" :
+#     print("eshtb")
+#     pasword=input("pasword:")
+
+# print("corect")  
---
+++ test.py
@@ -0,0 +1,77 @@
+# print("...............")
+# #role
+# print("masahat motavazi azla chegadr ast?")
+# x=float(input("height:")) *float(input("ryle:"))
+# x=round(x,2)
+# print(f"area : {x}cm")
+# print("...............")
+# #role
+# print("masahat motavazi azla chegadr ast?")
+# y=float(input("height:")) * 3.7
+# y=round(y,2)
+# print(f"area : {y}cm")
+# print("........number........")
+# my_list=[int(input()),int(input()),int(input()),int(input()),int(input())]
+# my_list.sort()
+# print(*my_list,sep='/')
+# print(".......num2.......")
+# my_list=[int(input()),int(input()),int(input()),int(input()),int(input())]
+# print(sorted(my_list))
+# print("...................................")
+# import random
+# from secrets import randbits
+# print("rock")
+# print("paper")
+# print("scor")
+# num=random.randint(0,2)
+# if num==0:
+#     cam="rock"
+# elif num==1:
+#     cam="paper"
+# elif num==2:
+#     cam="scor"    
+# play1=input("bazikonplayer1:").lower()
+# play2=cam
+# print(f"bazikonplayer2:{play2}")
+# if play1==play2:
+#     print("mosavi")
+# elif play1=="rock"  :
+#     if play2=="paper"  :
+#         print("player2wins!...")
+#     elif play2=="scor" :
+#         print("player1wins!...")   
+# elif play1=="paper"  :
+#     if play2=="rock"  :
+#         print("player1wins!...")
+#     elif play2=="scor" :
+#         print("player2wins!...")         
+# elif play1=="scor"  :
+#     if play2=="rock"  :
+#         print("player2wins!...")
+#     elif play2=="paper" :
+#         print("player1wins!...") 
+# else:
+#     print("problem!")
+# print(".........*.........")
+# for num in range(1,6) :
+#       print("*" *num)
+# print("........................")
+
+# for num in range(5,0,-1) :
+#     print("*" *num)
+# for num in range(5,0,-1) :
+#     print("*" *num)
+# print(".....tam......")
+# n=int(input("ent"))
+# for i in range(n):
+#     for j in range (i):
+#         print("*",end=" ")
+#     print()  
+
+# n=int(input("ent"))
+# for i in range(n):
+#     for j in range (i):
+#         print(" ",end=" ")
+#     for j in range (i,n):
+#         print("*",end=" ")    
+#     print()      
