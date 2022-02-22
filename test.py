d322d070ad20552f1e7885519f4795e120b5c25a
fatemeh-samei <fati09156090@gmail.com>
2022-02-22 11:03:00 +0330

    first commit

--- test.py
+++ test.py
@@ -69,9 +69,9 @@
 #     print()  
 
 # n=int(input("ent"))
-# for i in range(n):
-#     for j in range (i):
+# for i in range(n ,1,-1):
+#     for j in range (i-1):
 #         print(" ",end=" ")
-#     for j in range (i,n):
-#         print("*",end=" ")    
+#     for j in range (n,i-1,-1):
+#         print("*",end=" ") 
 #     print()      
